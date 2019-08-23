6e92fc8616783daa93a836ea3207e2404b6f8c44
Gede Lumbung <gedesumawijaya@gmail.com>
2019-08-23 18:46:33 +0800

    allowed CORS origin

--- app/main.go
+++ app/main.go
@@ -46,7 +46,14 @@ func main() {
 	http.ListenAndServe(":9090", nil)
 }
 
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+        (*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func getCurrentActiveUsers(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
 	if r.Method == "GET" {
 		startDate, eSd := r.URL.Query()["start_date"]
 		if !eSd || len(startDate[0]) < 1 {
